aab12c2a27eb826dc73e8a5fcc408d90ef8bd28c
taeuk <nous178@naver.com>
2021-07-24 19:42:37 +0900

    chore: Bundle 변경 및 apple, kakao Login 테스트 완료

--- Picme/PicmeUITests/PicmeUITests.swift
+++ Picme/PicmeUITests/PicmeUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class dnd_5th_1_iOSUITests: XCTestCase {
+class PicmeUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
