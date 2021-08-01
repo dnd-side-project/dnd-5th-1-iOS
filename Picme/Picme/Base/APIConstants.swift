4fde34bc82203f5080d7e3fc74991946ee84a993
taeuk <nous178@naver.com>
2021-08-01 18:26:12 +0900

    refactor: 파일 폴더화 및 APIConstant 수정

--- Picme/Picme/Base/APIConstants.swift
+++ Picme/Picme/Base/APIConstants.swift
@@ -10,17 +10,22 @@ import Foundation
 // MARK: 해당 파일 안에는 API 관련 상수들을 추가해주세요!
 
 struct APIConstants {
+    
     // MARK: - Headers
     
-    static let authorization = "Authorization"
-    static let accept = "Accept"
-    static let auth = "x-auth-token"
-    static let contentType = "Content-Type"
+    enum Header: String {
+        case authorization = "Authorization"
+        case accept = "Accept"
+        case auth = "x-auth-token"
+        case contentType = "Content-Type"
+    }
 
     // MARK: - Values
     
-    static let applicationJSON = "application/json"
-    static let formEncoded = "application/x-www-form-urlencoded"
+    enum Values: String {
+        case applicationJSON = "application/json"
+        case formEncoded = "application/x-www-form-urlencoded"
+    }
 
     // MARK: - Keys
     
@@ -29,8 +34,8 @@ struct APIConstants {
     // MARK: - URLs
     
     // Base URL
-    static let baseURL = "http://2d63c4581cec.ngrok.io/"
-    static let urlVersion = "v1/"
+    private static let baseURL = "http://2d63c4581cec.ngrok.io/"
+    private static let urlVersion = "v1/"
     
     // MARK: - /Main URLs
     
