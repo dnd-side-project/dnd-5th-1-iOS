013eb674a525a0f10455620db119b9cd045ea6d3
taeuk <nous178@naver.com>
2021-08-09 10:58:38 +0900

    feat: 이미지 업로드 확인창 구현

--- Picme/Picme/View/Controller/VoteCreate/ImageUploadViewContoller.swift
+++ Picme/Picme/View/Controller/VoteCreate/ImageUploadViewContoller.swift
@@ -19,20 +19,40 @@ class ImageUploadViewContoller: BaseViewContoller {
     
     let customs = StepView(stepText: "STEP 1", title: "투표받고 싶은 사진을 올려주세요!")
     
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.backgroundColor = .clear
+        return collection
+    }()
+    
+    let nextButton: UIButton = {
+        $0.backgroundColor = .mainColor(.pink)
+        $0.layer.cornerRadius = 10
+        $0.setTitle("확인", for: .normal)
+        $0.setTitleColor(.textColor(.text100), for: .normal)
+        return $0
+    }(UIButton(type: .system))
+    
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        uploadViewModel?.buttonState.bindAndFire(listener: { state in
-            self.stackView.isHidden = state
-        })
-        
-        
-        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerCell(ImageUploadCell.self)
     }
     
     @IBAction func testAction(_ sender: UIButton) {
         uploadViewModel?.buttonState.value = true
+    }
+    
+    func viewState(_ state: Bool) {
+        self.stackView.isHidden = state
+        self.collectionView.isHidden = !state
+        self.nextButton.isHidden = !state
     }
 }
 
@@ -43,8 +63,14 @@ extension ImageUploadViewContoller {
     override func setProperties() {
         
         view.addSubview(customs)
+        view.addSubview(collectionView)
+        view.addSubview(nextButton)
+        
         customs.clipsToBounds = true
         customs.backgroundColor = .solidColor(.solid12)
+        
+        collectionView.isHidden = true
+        nextButton.isHidden = true
         
         // NavigationBar
         if let navBar = navigationController?.navigationBar {
@@ -54,6 +80,19 @@ extension ImageUploadViewContoller {
             navBar.topItem?.title = "사진 업로드"
             navBar.titleTextAttributes = [.foregroundColor: UIColor.textColor(.text100)]
         }
+        
+        nextButton.addTarget(self, action: #selector(onePickChoise(_:)), for: .touchUpInside)
+    }
+    
+    @objc func onePickChoise(_ sender: UIButton) {
+         
+    }
+    
+    override func setBind() {
+        
+        uploadViewModel?.buttonState.bindAndFire(listener: { state in
+            self.viewState(state)
+        })
     }
     
     override func setConfiguration() {
@@ -71,13 +110,39 @@ extension ImageUploadViewContoller {
     override func setConstraints() {
         
         testButton.translatesAutoresizingMaskIntoConstraints = false
-        testButton.widthAnchor.constraint(equalToConstant: view.frame.width / 2.5).isActive = true
-        testButton.heightAnchor.constraint(equalToConstant: 52).isActive = true
+        testButton.widthAnchor.constraint(equalToConstant: view.frame.width / 2.5)
+            .isActive = true
+        testButton.heightAnchor.constraint(equalToConstant: 52)
+            .isActive = true
         
         customs.translatesAutoresizingMaskIntoConstraints = false
-        customs.topAnchor.constraint(equalTo: progressBar.bottomAnchor, constant: 14).isActive = true
-        customs.leadingAnchor.constraint(equalTo: progressBar.leadingAnchor).isActive = true
-        customs.trailingAnchor.constraint(equalTo: progressBar.trailingAnchor).isActive = true
-        customs.heightAnchor.constraint(equalToConstant: 72).isActive = true
+        customs.topAnchor.constraint(equalTo: progressBar.bottomAnchor, constant: 14)
+            .isActive = true
+        customs.leadingAnchor.constraint(equalTo: progressBar.leadingAnchor)
+            .isActive = true
+        customs.trailingAnchor.constraint(equalTo: progressBar.trailingAnchor)
+            .isActive = true
+        customs.heightAnchor.constraint(equalToConstant: 72)
+            .isActive = true
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.topAnchor.constraint(equalTo: customs.bottomAnchor, constant: 14)
+            .isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: progressBar.leadingAnchor)
+            .isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: progressBar.trailingAnchor)
+            .isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: nextButton.topAnchor, constant: -20)
+            .isActive = true
+        
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -28)
+            .isActive = true
+        nextButton.leadingAnchor.constraint(equalTo: progressBar.leadingAnchor)
+            .isActive = true
+        nextButton.trailingAnchor.constraint(equalTo: progressBar.trailingAnchor)
+            .isActive = true
+        nextButton.heightAnchor.constraint(equalToConstant: 52)
+            .isActive = true
     }
 }
