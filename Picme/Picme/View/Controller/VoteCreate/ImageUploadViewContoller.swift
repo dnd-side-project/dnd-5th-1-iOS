2a82fb3474b9adbc850a68fdca2a2de86e341146
taeuk <nous178@naver.com>
2021-08-12 19:08:16 +0900

    refactor: STEP 1 CollectionView 제거 및 라벨, 버튼명 변경

--- Picme/Picme/View/Controller/VoteCreate/ImageUploadViewContoller.swift
+++ Picme/Picme/View/Controller/VoteCreate/ImageUploadViewContoller.swift
@@ -13,29 +13,12 @@ class ImageUploadViewContoller: BaseViewContoller {
     // MARK: - Properties
     var uploadViewModel: ImageUploadViewModel? = ImageUploadViewModel()
     
-    @IBOutlet weak var testButton: UIButton!
-    @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var uploadButton: UIButton!
+    @IBOutlet weak var uploadLabel: UILabel!
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var progressBar: UIProgressView!
     
     let stepView = StepView(stepText: "STEP 1", title: "투표받고 싶은 사진을 올려주세요!")
-    
-    let collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        
-        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collection.backgroundColor = .clear
-        return collection
-    }()
-    
-    let nextButton: UIButton = {
-        $0.backgroundColor = .mainColor(.pink)
-        $0.layer.cornerRadius = 10
-        $0.setTitle("확인", for: .normal)
-        $0.setTitleColor(.textColor(.text100), for: .normal)
-        return $0
-    }(UIButton(type: .system))
     
     // YPImagePicker Properties
     var selectedItems = [YPMediaItem]()
@@ -45,9 +28,6 @@ class ImageUploadViewContoller: BaseViewContoller {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.registerCell(ImageUploadCell.self)
     }
     
     @IBAction func testAction(_ sender: UIButton) {
@@ -98,12 +78,6 @@ class ImageUploadViewContoller: BaseViewContoller {
         
         present(picker, animated: true, completion: nil)
     }
-    
-    func viewState(_ state: Bool) {
-        self.stackView.isHidden = state
-        self.collectionView.isHidden = !state
-        self.nextButton.isHidden = !state
-    }
 }
 
 // MARK: - UI
@@ -113,15 +87,10 @@ extension ImageUploadViewContoller {
     override func setProperties() {
         
         view.addSubview(stepView)
-        view.addSubview(collectionView)
-        view.addSubview(nextButton)
         
         stepView.clipsToBounds = true
         stepView.backgroundColor = .solidColor(.solid12)
         stepView.layer.cornerRadius = 10
-        
-        collectionView.isHidden = true
-        nextButton.isHidden = true
         
         // NavigationBar
         if let navBar = navigationController?.navigationBar {
@@ -131,40 +100,26 @@ extension ImageUploadViewContoller {
             navBar.topItem?.title = "사진 업로드"
             navBar.titleTextAttributes = [.foregroundColor: UIColor.textColor(.text100)]
         }
-        
-        nextButton.addTarget(self, action: #selector(onePickChoise(_:)), for: .touchUpInside)
-    }
-    
-    @objc func onePickChoise(_ sender: UIButton) {
-        guard let onePickVC = storyboard?.instantiateViewController(withIdentifier: "OnePickViewController") as? OnePickViewController else { return }
-        navigationController?.pushViewController(onePickVC, animated: true)
-    }
-    
-    override func setBind() {
-        
-        uploadViewModel?.buttonState.bindAndFire(listener: { state in
-            self.viewState(state)
-        })
     }
     
     override func setConfiguration() {
         
         view.backgroundColor = .solidColor(.solid0)
         
-        testLabel.textColor = .textColor(.text100)
+        uploadLabel.textColor = .textColor(.text100)
         
-        testButton.layer.cornerRadius = 10
-        testButton.setTitleColor(.textColor(.text100), for: .normal)
-        testButton.backgroundColor = .mainColor(.pink)
+        uploadButton.layer.cornerRadius = 10
+        uploadButton.setTitleColor(.textColor(.text100), for: .normal)
+        uploadButton.backgroundColor = .mainColor(.pink)
         
     }
     
     override func setConstraints() {
         
-        testButton.translatesAutoresizingMaskIntoConstraints = false
-        testButton.widthAnchor.constraint(equalToConstant: view.frame.width / 2.5)
+        uploadButton.translatesAutoresizingMaskIntoConstraints = false
+        uploadButton.widthAnchor.constraint(equalToConstant: view.frame.width / 2.5)
             .isActive = true
-        testButton.heightAnchor.constraint(equalToConstant: 52)
+        uploadButton.heightAnchor.constraint(equalToConstant: 52)
             .isActive = true
         
         stepView.translatesAutoresizingMaskIntoConstraints = false
@@ -175,26 +130,6 @@ extension ImageUploadViewContoller {
         stepView.trailingAnchor.constraint(equalTo: progressBar.trailingAnchor)
             .isActive = true
         stepView.heightAnchor.constraint(equalToConstant: 72)
-            .isActive = true
-        
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.topAnchor.constraint(equalTo: stepView.bottomAnchor, constant: 14)
-            .isActive = true
-        collectionView.leadingAnchor.constraint(equalTo: progressBar.leadingAnchor)
-            .isActive = true
-        collectionView.trailingAnchor.constraint(equalTo: progressBar.trailingAnchor)
-            .isActive = true
-        collectionView.bottomAnchor.constraint(equalTo: nextButton.topAnchor, constant: -20)
-            .isActive = true
-        
-        nextButton.translatesAutoresizingMaskIntoConstraints = false
-        nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -28)
-            .isActive = true
-        nextButton.leadingAnchor.constraint(equalTo: progressBar.leadingAnchor)
-            .isActive = true
-        nextButton.trailingAnchor.constraint(equalTo: progressBar.trailingAnchor)
-            .isActive = true
-        nextButton.heightAnchor.constraint(equalToConstant: 52)
             .isActive = true
     }
 }
