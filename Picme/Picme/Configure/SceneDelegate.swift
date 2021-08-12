f86f154793796465e8ea61c0278a07349097ad82
taeuk <nous178@naver.com>
2021-08-12 18:56:58 +0900

    chore: SPM_Snapkit, YPImagePicker 추가

--- Picme/Picme/Configure/SceneDelegate.swift
+++ Picme/Picme/Configure/SceneDelegate.swift
@@ -26,9 +26,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         
-        let mainStoryboard = UIStoryboard(name: "ImageUpload", bundle: nil)
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let mainViewController = mainStoryboard.instantiateViewController(withIdentifier:
-                                                                            "ImageUploadViewContoller")
+                                                                            "TabBarController")
         mainViewController.modalPresentationStyle = .fullScreen
         self.window?.rootViewController = mainViewController
         self.window?.makeKeyAndVisible()
