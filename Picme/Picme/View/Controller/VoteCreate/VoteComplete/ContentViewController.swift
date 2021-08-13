ee6256cd52c7d846b4802b197a7a9dca269f8049
taeuk <nous178@naver.com>
2021-08-13 17:20:54 +0900

    feat: 마감시간 DatePicker 추가

--- Picme/Picme/View/Controller/VoteCreate/VoteComplete/ContentViewController.swift
+++ Picme/Picme/View/Controller/VoteCreate/VoteComplete/ContentViewController.swift
@@ -19,24 +19,73 @@ class ContentViewController: BaseViewContoller {
     @IBOutlet weak var voteTextView: UITextView!
     
     // 투표 마감시간
-    @IBOutlet weak var voteEndDateTimeLabel: UILabel!
-    @IBOutlet weak var voteEndView: UIView!
-    @IBOutlet weak var voteEndDate: UILabel!
+    @IBOutlet weak var voteEndDateTextLabel: UILabel!
+    @IBOutlet weak var voteEndDateTextfield: UITextField!
     
     @IBOutlet weak var upStackView: UIStackView!
     @IBOutlet weak var bottomStackView: UIStackView!
     
     @IBOutlet weak var registVoteButton: UIButton!
     
+    lazy var datePicker: UIDatePicker = {
+        $0.locale = Locale(identifier: "en_kr")
+        $0.datePickerMode = .dateAndTime
+        $0.backgroundColor = .white
+        
+        if #available(iOS 13.4, *) {
+            $0.preferredDatePickerStyle = .wheels
+        } else {
+            // Fallback on earlier versions
+        }
+        return $0
+    }(UIDatePicker())
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        voteEndDateTextfield.inputView = datePicker
+        datePicker.addTarget(self, action: #selector(dateChanged(_:)), for: .valueChanged)
+//        datePickerToolBar()
+    }
+    @objc func dateChanged(_ sender: UIDatePicker) {
+        let dateformatter = DateFormatter()
+        dateformatter.locale = Locale(identifier: "ko_KR")
+        dateformatter.dateFormat = "yy/MM/dd hh:mm"
+        
+        let selectDate: String = dateformatter.string(from: sender.date)
+        voteEndDateTextfield.text = selectDate
     }
 
     @IBAction func registVote(_ sender: UIButton) {
         print("Regist")
+    }
+    
+    func datePickerToolBar() {
+        let toolbar = UIToolbar()
+        toolbar.barStyle = .default
+        toolbar.isTranslucent = true
+        toolbar.tintColor = .white
+        toolbar.sizeToFit()
+        
+        let done = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(toolBarDoneButton(_:)))
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancel = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(toolBarCancelButton(_:)))
+        
+        toolbar.setItems([cancel, flexibleSpace, done], animated: true)
+        toolbar.isUserInteractionEnabled = true
+        
+        voteEndDateTextfield.inputAccessoryView = toolbar
+    }
+    
+    @objc func toolBarDoneButton(_ sender: UIButton) {
+        print("DONE")
+        voteEndDateTextfield.resignFirstResponder()
+    }
+    
+    @objc func toolBarCancelButton(_ sender: UIButton) {
+        voteEndDateTextfield.text = nil
+        voteEndDateTextfield.resignFirstResponder()
     }
 }
 
@@ -46,13 +95,6 @@ extension ContentViewController {
 
     override func setProperties() {
         
-    }
-    
-    override func setConfiguration() {
-        
-        view.backgroundColor = .solidColor(.solid0)
-        
-        view.addSubview(stepView)
         stepView.clipsToBounds = true
         stepView.backgroundColor = .solidColor(.solid12)
         stepView.layer.cornerRadius = 10
@@ -63,11 +105,11 @@ extension ContentViewController {
         voteTextView.backgroundColor = .solidColor(.solid12)
         voteTextView.layer.cornerRadius = 10
         
-        voteEndDateTimeLabel.textColor = .mainColor(.pink)
-        voteEndView.backgroundColor = .solidColor(.solid12)
-        voteEndView.layer.cornerRadius = 10
-        voteEndDate.text = "21/07/14 00:00"
-        voteEndDate.textColor = .textColor(.text100)
+        voteEndDateTextLabel.textColor = .mainColor(.pink)
+        voteEndDateTextfield.backgroundColor = .solidColor(.solid12)
+        voteEndDateTextfield.textAlignment = .center
+        voteEndDateTextfield.text = "21/07/15 00:00"
+        voteEndDateTextfield.textColor = .white
         
         registVoteButton.backgroundColor = .solidColor(.solid26)
         registVoteButton.setTitleColor(.textColor(.text50), for: .normal)
@@ -75,45 +117,41 @@ extension ContentViewController {
         registVoteButton.layer.cornerRadius = 10
     }
     
+    override func setConfiguration() {
+        
+        view.addSubview(stepView)
+        view.backgroundColor = .solidColor(.solid0)
+        
+    }
+    
     override func setConstraints() {
         
-        stepView.translatesAutoresizingMaskIntoConstraints = false
-        stepView.topAnchor.constraint(equalTo: progressBar.bottomAnchor, constant: 14)
-            .isActive = true
-        stepView.leadingAnchor.constraint(equalTo: progressBar.leadingAnchor)
-            .isActive = true
-        stepView.trailingAnchor.constraint(equalTo: progressBar.trailingAnchor)
-            .isActive = true
-        stepView.heightAnchor.constraint(equalToConstant: 72)
-            .isActive = true
+        stepView.snp.makeConstraints {
+            $0.top.equalTo(progressBar.snp.bottom).offset(14)
+            $0.leading.equalTo(progressBar.snp.leading)
+            $0.trailing.equalTo(progressBar.snp.trailing)
+            $0.height.equalTo(72)
+        }
         
-        upStackView.translatesAutoresizingMaskIntoConstraints = false
-        upStackView.topAnchor.constraint(equalTo: stepView.bottomAnchor, constant: 100)
-            .isActive = true
-        upStackView.leadingAnchor.constraint(equalTo: progressBar.leadingAnchor)
-            .isActive = true
-        upStackView.trailingAnchor.constraint(equalTo: progressBar.trailingAnchor)
-            .isActive = true
-
-        bottomStackView.translatesAutoresizingMaskIntoConstraints = false
-        bottomStackView.topAnchor.constraint(equalTo: upStackView.bottomAnchor, constant: 40)
-            .isActive = true
-        bottomStackView.leadingAnchor.constraint(equalTo: stepView.leadingAnchor)
-            .isActive = true
-        bottomStackView.trailingAnchor.constraint(equalTo: stepView.trailingAnchor)
-            .isActive = true
-        bottomStackView.bottomAnchor.constraint(lessThanOrEqualTo: registVoteButton.topAnchor,
-                                                constant: -30)
-            .isActive = true
+        upStackView.snp.makeConstraints {
+            $0.top.equalTo(stepView.snp.bottom).offset(100)
+            $0.leading.equalTo(progressBar.snp.leading)
+            $0.trailing.equalTo(progressBar.snp.trailing)
+        }
         
-        registVoteButton.translatesAutoresizingMaskIntoConstraints = false
-        registVoteButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100)
-            .isActive = true
-        registVoteButton.leadingAnchor.constraint(equalTo: progressBar.leadingAnchor)
-            .isActive = true
-        registVoteButton.trailingAnchor.constraint(equalTo: progressBar.trailingAnchor)
-            .isActive = true
-        registVoteButton.heightAnchor.constraint(equalToConstant: 52)
-            .isActive = true
+        bottomStackView.snp.makeConstraints {
+            $0.top.equalTo(upStackView.snp.bottom).offset(40)
+            $0.leading.equalTo(stepView.snp.leading)
+            $0.trailing.equalTo(stepView.snp.trailing)
+            $0.bottom.lessThanOrEqualTo(registVoteButton.snp.top).offset(-30)
+        }
+        
+        registVoteButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.snp.bottom).offset(-100)
+            $0.leading.equalTo(progressBar.snp.leading)
+            $0.trailing.equalTo(progressBar.snp.trailing)
+            $0.height.equalTo(52)
+        }
+        
     }
 }
