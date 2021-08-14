a570c852ec9d38ac80e991775e2066e4d9814b73
Minha Kwon <minha61@naver.com>
2021-08-15 05:35:52 +0900

    refactor : VoteDetailListModel - SwiftLint 오류 수정

--- Picme/Picme/Model/VoteDetail/VoteDetailListModel.swift
+++ Picme/Picme/Model/VoteDetail/VoteDetailListModel.swift
@@ -10,7 +10,7 @@ import Foundation
 struct VoteDetailListModel: Codable {
     var detailList: [VoteDetailModel]?
     
-    init(detailList: [VoteDetailModel]){
+    init(detailList: [VoteDetailModel]) {
         self.detailList = detailList
     }
     
