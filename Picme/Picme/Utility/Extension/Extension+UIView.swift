9a920dfa185c8e873e0a759944cec9945709d4cd
Minha Kwon <minha61@naver.com>
2021-08-15 06:29:45 +0900

    feat : Extensoin+UIView 파일 추가

--- Picme/Picme/Utility/Extension/Extension+UIView.swift
+++ Picme/Picme/Utility/Extension/Extension+UIView.swift
@@ -10,31 +10,31 @@ import UIKit
 extension UIView {
     
     @IBInspectable var borderWidth: CGFloat {
-        set {
-            layer.borderWidth = newValue
-        }
         get {
             return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
         }
     }
     
     @IBInspectable var cornerRadius: CGFloat {
-        set {
-            layer.cornerRadius = newValue
-        }
         get {
             return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
         }
     }
     
     @IBInspectable var borderColor: UIColor? {
-        set {
-            guard let uiColor = newValue else { return }
-            layer.borderColor = uiColor.cgColor
-        }
         get {
             guard let color = layer.borderColor else { return nil }
             return UIColor(cgColor: color)
+        }
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
         }
     }
     
