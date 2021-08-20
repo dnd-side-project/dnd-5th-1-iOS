389faef124b7f62f15698420a168ff50b62f8bce
Minha Kwon <minha61@naver.com>
2021-08-20 17:00:58 +0900

    feat : 투표 생성, 게시물 삭제 Service 코드 작성 및 APIConstants 수정

--- Picme/Picme/Network/VoteDetail/VoteDetailService.swift
+++ Picme/Picme/Network/VoteDetail/VoteDetailService.swift
@@ -10,14 +10,67 @@ import Alamofire
 
 protocol VoteDetailServiceProtocol: AnyObject {
     func getVoteDetail(postId: Int, completion: @escaping ((NetworkResult<Any>) -> Void))
+    func createVote(postId: String, imageId: String, category: String, completion: @escaping ((NetworkResult<Any>) -> Void))
+    func deletePost(postId: String, imageId: String, completion: @escaping ((NetworkResult<Any>) -> Void))
 }
 
 class VoteDetailService: VoteDetailServiceProtocol {
+    
+    // MARK: - 투표 상세 조회
+    
     func getVoteDetail(postId: Int, completion: @escaping ((NetworkResult<Any>) -> Void)) {
         let URL = APIConstants.Post.postRetrieve(postID: String(postId)).urlString
         
         let dataRequest = AF.request(URL,
                                      method: .get,
+                                     parameters: nil,
+                                     encoding: JSONEncoding.default,
+                                     headers: nil)
+        
+        dataRequest.responseData { dataResponse in
+            switch dataResponse.result {
+            case .success:
+                guard let statusCode = dataResponse.response?.statusCode else {return}
+                guard let value = dataResponse.value else {return}
+                let networkResult = self.judgeStatus(by: statusCode, value)
+                completion(networkResult)
+            case .failure: completion(.pathErr)
+            }
+        }
+    }
+    
+    // MARK: - 투표 생성
+    
+    func createVote(postId: String, imageId: String, category: String, completion: @escaping ((NetworkResult<Any>) -> Void)) {
+        let URL = APIConstants.Vote.createVote(postID: postId, postImageID: imageId).urlString
+        let body: [String : String] = [
+            "category" : category]
+        
+        let dataRequest = AF.request(URL,
+                                     method: .post,
+                                     parameters: body,
+                                     encoding: JSONEncoding.default,
+                                     headers: nil)
+        
+        dataRequest.responseData { dataResponse in
+            switch dataResponse.result {
+            case .success:
+                guard let statusCode = dataResponse.response?.statusCode else {return}
+                guard let value = dataResponse.value else {return}
+                let networkResult = self.judgeStatus(by: statusCode, value)
+                completion(networkResult)
+            case .failure: completion(.pathErr)
+            }
+        }
+    }
+    
+    // MARK: - 게시글 삭제
+    
+    func deletePost(postId: String, imageId: String, completion: @escaping ((NetworkResult<Any>) -> Void)) {
+        let URL = APIConstants.Post.deletePost(postID: postId).urlString
+        
+        let dataRequest = AF.request(URL,
+                                     method: .post,
                                      parameters: nil,
                                      encoding: JSONEncoding.default,
                                      headers: nil)
