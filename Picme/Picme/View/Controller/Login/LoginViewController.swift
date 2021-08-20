31422334a2a1cf9eed228dca6e274ff5fa64a72c
taeuk <nous178@naver.com>
2021-08-20 23:56:22 +0900

    refactor: 카카오 로그인 버튼 UI 변경

--- Picme/Picme/View/Controller/Login/LoginViewController.swift
+++ Picme/Picme/View/Controller/Login/LoginViewController.swift
@@ -19,9 +19,16 @@ class LoginViewController: BaseViewContoller {
     @IBOutlet weak var unLoginButton: UIButton!
     
     var loginViewModel: LoginViewModel? = LoginViewModel()
+ 
+    // 카카오 로고
+    private let kakaoLogoImage: UIImageView = {
+        $0.image = UIImage(named: "kakaosymbol")
+        $0.contentMode = .scaleAspectFill
+        return $0
+    }(UIImageView())
     
     // 애플 로고
-    let appleLogoImage: UIImageView = {
+    private let appleLogoImage: UIImageView = {
         $0.image = UIImage(named: "Black Logo Large")
         $0.contentMode = .scaleAspectFill
         return $0
@@ -79,8 +86,16 @@ extension LoginViewController {
         appleLoginButton.layer.cornerRadius = 10
         appleLoginButton.backgroundColor = .white
         appleLoginButton.setTitleColor(.solidColor(.solid0), for: .normal)
+        appleLoginButton.titleLabel?.font = .kr(.bold, size: 16)
         appleLoginButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 0)
         appleLoginButton.addSubview(appleLogoImage)
+        
+        kakaoLoginButton.layer.cornerRadius = 10
+        kakaoLoginButton.backgroundColor = UIColor(red: 254/255, green: 229/255, blue: 0/255, alpha: 1.0)
+        kakaoLoginButton.setTitleColor(.solidColor(.solid0), for: .normal)
+        kakaoLoginButton.titleLabel?.font = .kr(.bold, size: 16)
+        kakaoLoginButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 0)
+        kakaoLoginButton.addSubview(kakaoLogoImage)
     }
     
     override func setConfiguration() {
@@ -93,6 +108,12 @@ extension LoginViewController {
     }
     
     override func setConstraints() {
+        
+        kakaoLogoImage.snp.makeConstraints {
+            $0.leading.equalTo(kakaoLoginButton.snp.leading).offset(15)
+            $0.centerY.equalTo(kakaoLoginButton.snp.centerY)
+            $0.width.equalTo(24)
+        }
         
         appleLogoImage.snp.makeConstraints {
             $0.leading.equalTo(appleLoginButton.snp.leading).offset(15)
