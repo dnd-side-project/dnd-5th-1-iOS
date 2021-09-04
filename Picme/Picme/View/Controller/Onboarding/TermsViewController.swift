2106f4d5570276db3fbce70cd187eb669a0bdeca
taeuk <nous178@naver.com>
2021-09-04 17:17:57 +0900

    feat: 로그인전 약관동의 UI 구현

---
+++ Picme/Picme/View/Controller/Onboarding/TermsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TermsViewController.swift
+//  Picme
+//
+//  Created by taeuk on 2021/09/04.
+//
+
+import UIKit
+
+class TermsViewController: BaseViewContoller {
+
+    @IBOutlet weak var allCheckButton: UIButton!
+    @IBOutlet weak var termsCheckButton: UIButton!
+    @IBOutlet weak var policyCheckButton: UIButton!
+    
+    @IBOutlet weak var seeTermsButton: UIButton!
+    @IBOutlet weak var seePolicyButton: UIButton!
+    
+    @IBOutlet weak var agreeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func agreeAction(_ sender: UIButton) {
+        
+    }
+}
+
+extension TermsViewController {
+    
+}
