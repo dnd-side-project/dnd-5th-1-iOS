eca08cea74847158db993cac0345e79826b668f0
Minha Kwon <minha61@naver.com>
2021-09-27 00:55:02 +0900

    refactor: MainModel 주석 삭제

--- Picme/Picme/ViewModel/Main/MainViewModel.swift
+++ Picme/Picme/ViewModel/Main/MainViewModel.swift
@@ -35,36 +35,15 @@ class MainViewModel {
         self.delegate = delegate
     }
     
-    //    init(service: MainServiceProtocol, dataSource: GenericDataSource<MainModel>?, delegate: MainViewModelDelegate) {
-    //        self.service = service
-    //        self.dataSource = dataSource
-    //        self.delegate = delegate
-    //    }
-    
-    //    init(service: MainServiceProtocol, dataSource: GenericDataSource<MainModel>?) {
-    //        self.service = service
-    //        self.dataSource = dataSource
-    //    }
-    
-    ///
     var totalCount: Int {
         return total
     }
-    //
-    //    var currentCount: Int {
-    //        return dataSource?.data.value.count ?? 0
-    //        // return moderators.count
-    //    }
-    //
-    //    func mainListIndex(at index: Int) -> MainModel {
-    //        return (dataSource?.data.value[index])!
-    //    }
     
     var currentCount: Int {
         return mainList.count
     }
     
-    func moderator(at index: Int) -> MainModel {
+    func mainModel(at index: Int) -> MainModel {
         return mainList[index]
     }
     
@@ -77,8 +56,6 @@ class MainViewModel {
         }
         
         service.getMainList(page: currentPage, completion: { [weak self] (response) in
-            print("* get Main List")
-            
             DispatchQueue.main.async {
                 switch response {
                 case .success(let data):
@@ -89,31 +66,19 @@ class MainViewModel {
                         
                         self?.total = responseData.total
                         
-                        print("* total : \(String(describing: self?.total))")
                         self?.dataSource?.data.value.append(contentsOf: responseData.mainList)
                         
                         self?.mainList.append(contentsOf: responseData.mainList)
                         
-                        print("* in response data 개수!?! \(responseData.mainList.count)")
-                        
-                        // 3
-                        if self?.page ?? 0 >= 1 {
-                            print("* page > 1 \(String(describing: self?.page))")
+                        if self?.page ?? 0 >= 1 { // 그 이외의 페이지일 경우
                             let indexPathsToReload = self?.calculateIndexPathsToReload(from: responseData.mainList)
                             self?.delegate?.onFetchCompleted(with: indexPathsToReload)
-                        } else {
-                            print("* page else \(String(describing: self?.page))")
+                        } else { // 첫 페이지일 경우
                             self?.delegate?.onFetchCompleted(with: .none)
                             self?.page += 1
                         }
                         
                     }
-                    
-                // 페이징 전 코드
-                // if let mainData = data as? [MainModel] {
-                // self?.dataSource?.data.value = mainData
-                // }
-                
                 case .requestErr(let message):
                     print("requestERR", message)
                 case .pathErr:
@@ -128,12 +93,9 @@ class MainViewModel {
     }
     
     private func calculateIndexPathsToReload(from newMainList: [MainModel]) -> [IndexPath] {
-        print("* calculate Index Paths To Reload")
-        // let startIndex = dataSource?.data.value.count ?? 0 - newMainList.count
         let startIndex = mainList.count - newMainList.count
-        
         let endIndex = startIndex + newMainList.count
-        print("* start : \(startIndex) + end : \(endIndex)")
+        
         return (startIndex..<endIndex).map { IndexPath(row: $0, section: 0) }
     }
     
