2f0b0760af59be293d6f37406fd20e2debd012e1
taeuk <nous178@naver.com>
2021-10-11 22:35:55 +0900

    feat: splash 2초 지연

--- Picme/Picme/Configure/AppDelegate.swift
+++ Picme/Picme/Configure/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UITabBarControllerDelegat
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        sleep(2)
         
         KakaoSDKCommon.initSDK(appKey: "d1705396377c942afa5628eba176683f")
         
